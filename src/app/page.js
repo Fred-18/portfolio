df979836a4f3912080814b17572cefe78fbefd06
fred <ton_email@example.com>
2024-07-15 19:08:09 +0200

    feat(Update): :sparkles: add about me component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import { Header } from "@/components/Header/Header";
+import { AboutMe } from "@/components/About_Me/AboutMe";
+
 export default function Home() {
   return (
     <>
       <Header />
+      <AboutMe/>
     </>
   );
 }
