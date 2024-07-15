eaf1cea3235e07eebe2bd6db85fe05f81674f020
fred <ton_email@example.com>
2024-07-15 19:04:38 +0200

    feat(Update): :art: update header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import s from "./style.module.css";
 export const Header = () => {
+  const firstName = "Fred";
+  const lastname = "Nobre";
+  const developer = "Web Developer";
+
   return (
     <>
       <div className={s.firstcontainer}>
         <div className={s.container}>
-          <h1 className={s.firstTitle}>Nobre</h1>
-          <h2 className={s.secondTitle}>Fred</h2>
-          <h3 className={s.thridTitle}>Web Developer</h3>
+          <h1 className={s.firstTitle}>{`${firstName}`}</h1>
+          <h2 className={s.secondTitle}>{`${lastname}`}</h2>
+          <h3 className={s.thridTitle}>{`${developer}`}</h3>
         </div>
         <img className={s.img_fred} src="asset/fredportfolio.jpg" />
       </div>
