9883f7e592bc243c1354964fa7d0379555491d7e
fred <frednobre18@gmail.com>
2025-07-08 11:41:48 +0200

    update burgerMenu🔧

--- components/burgerMenu/BurgerMenu.tsx
+++ components/burgerMenu/BurgerMenu.tsx
@@ -1,14 +1,25 @@
 import styles from './BurgerMenu.module.css'
 import Line from "../line/Line";
+import {JSX} from "react";
 
 export default function BurgerMenu() {
+
+
+    const lines: JSX.Element[] = [];
+
+    function repeatLines(): JSX.Element[] {
+        for (let i = 0; i < 3; i++) {
+            lines.push(<Line key={i}/>);
+        }
+        return lines
+    }
+
     return (
         <>
             <button className={styles.burgerMenu}>
-                <Line/>
-                <Line/>
-                <Line/>
+                {repeatLines()}
             </button>
         </>
     )
+
 }
