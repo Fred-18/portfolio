420da7f16056323509c108251fd8af8c07d452c0
fred <frednobre18@gmail.com>
2025-07-14 15:39:11 +0200

    update Hero components

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,19 +1,26 @@
 import styles from './Hero.module.css'
 import Image from "next/image";
 import BurgerMenu from "../burgerMenu/BurgerMenu";
+import NavigationBar from "../navigationBar/NavigationBar";
 
 export default function Hero() {
     return (
         <>
             <section className={styles.container}>
-                <div className={styles.secondeContainer}>
-                    <BurgerMenu/>
-                    <h1>HI THERE!</h1>
-                    <h2>I&#39;m Fred Nobre , a full-Stack developper<br/> based in<br/> France.</h2>
-                </div>
+                <BurgerMenu/>
+                <NavigationBar/>
                 <figure className={styles.imageWrapper}>
-                    <Image className={styles.turtel} src={"/assets/turtelWithbackground.svg"} alt={"fred turtel"}
-                           fill/>
+                    <div className={styles.secondeContainer}>
+                        <h1>HI THERE!</h1>
+                        <h2>I&#39;m Fred Nobre , a full-Stack developper<br/> based in<br/> France.</h2>
+
+                        <Image className={styles.turtel}
+                               src={"/assets/turtelWithbackground.svg"}
+                               alt={"fred turtel"}
+                               fill
+                               sizes="(max-width: 768px) 100vw, 50vw"/>
+
+                    </div>
                 </figure>
             </section>
         </>
