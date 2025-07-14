edfd18938d7d906a0ed311a5da8e6685d00aa9c4
fred <frednobre18@gmail.com>
2025-07-14 16:22:34 +0200

    add line components

--- components/line/Line.tsx
+++ components/line/Line.tsx
@@ -3,7 +3,7 @@ import styles from "./Line.module.css"
 export default function Line() {
     return (
         <svg className={styles.svg}>
-            <rect className={styles.rect} width="34" height="4" fill ="#54B47A"></rect>
+             <rect className={styles.rect} width="34" height="4" fill ="#54B47A"></rect>
         </svg>
 
     )
