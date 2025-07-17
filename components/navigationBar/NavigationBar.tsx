68e482b4df7f80c052ddbeb9642da8a81f664fe8
fred <frednobre18@gmail.com>
2025-07-17 19:42:02 +0200

    update navigationBar

--- components/navigationBar/NavigationBar.tsx
+++ components/navigationBar/NavigationBar.tsx
@@ -2,17 +2,15 @@ import styles from "./Navigation.module.css"
 
 export default function NavigationBar() {
 
-    return (
-        <nav className={styles.navigationBar}>
-            <ul className={styles.nopuce}>
-                <li>Home</li>
-                <li>About me</li>
-                <li>Project</li>
-                <li>Contact</li>
-                <li>Skills</li>
-                <li>Tools</li>
-                <li>Certificates</li>
-            </ul>
-        </nav>
-    )
+    return (<nav>
+        <ul id={"primary-navigation"} className={styles.primaryNavigation}>
+            <li className={"active"}><a className={styles.link} href={"home"}><span>Home</span></a></li>
+            <li><a className={styles.link} href={"aboutMe"}><span>About me</span></a></li>
+            <li><a className={styles.link} href={"projects"}><span>Projects</span></a></li>
+            <li><a className={styles.link} href={"Contact"}><span>Contact</span></a></li>
+            <li><a className={styles.link} href={"skills"}><span>Skills</span></a></li>
+            <li><a className={styles.link} href={"tools"}><span>Tools</span></a></li>
+            <li><a className={styles.link} href={"certificates"}><span>Certificates</span></a></li>
+        </ul>
+    </nav>)
 }
