cfe926042d9feb35916ab13af23b516523a02461
fred <frednobre18@gmail.com>
2025-07-17 19:40:48 +0200

    update hero

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -6,23 +6,26 @@ import NavigationBar from "../navigationBar/NavigationBar";
 export default function Hero() {
     return (
         <>
-            <section className={styles.container}>
+            <header className={styles.container}>
                 <BurgerMenu/>
                 <NavigationBar/>
                 <figure className={styles.imageWrapper}>
-                    <div className={styles.secondeContainer}>
-                        <h1>HI THERE!</h1>
-                        <h2>I&#39;m Fred Nobre , a full-Stack developper<br/> based in<br/> France.</h2>
+                    <div>
 
-                        <Image className={styles.turtel}
-                               src={"/assets/turtelWithbackground.svg"}
-                               alt={"fred turtel"}
-                               fill
-                               sizes="(max-width: 768px) 100vw, 50vw"/>
+                        <div className={styles.secondeContainer}>
+                            <h1>HI THERE!</h1>
+                            <h2>I&#39;m Fred Nobre , a full-Stack developper<br/> based in<br/> France.</h2>
 
+                            <Image className={styles.turtel}
+                                   src={"/assets/turtelWithbackground.svg"}
+                                   alt={"fred turtel"}
+                                   fill
+                                   sizes="(max-width: 768px) 100vw, 50vw"/>
+
+                        </div>
                     </div>
                 </figure>
-            </section>
+            </header>
         </>
     )
 }
