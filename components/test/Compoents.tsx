a53fed461bf6c2fb00f79c37a527d956c2ea728c
fred <frednobre18@gmail.com>
2025-09-01 20:22:13 +0200

    📂add new directory type

---
+++ components/test/Compoents.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {allowedDisplayValues} from "next/dist/compiled/@next/font/dist/constants";
+
+const Compoents = () => {
+    return (
+        <div>HELLO</div>
+    );
+};
+
+export default Compoents;
