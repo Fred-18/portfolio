58a097e715e240b00d498125554a114588c4ad59
Fred Nobre <frednobre18@gmail.com>
2025-09-08 10:55:03 +0200

    :construction: wip!

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,29 +1,26 @@
 import styles from './Hero.module.css'
-import Image from "next/image";
-import BurgerMenu from "../burgerMenu/BurgerMenu";
-import NavigationBar from "../navigationBar/NavigationBar";
+import { portfolioContent } from "@/content/portfolioContent";
 
 export default function Hero() {
     return (
         <>
             <header className={styles.container}>
                 <figure className={styles.imageWrapper}>
-                    <div>
-
-                        <div className={styles.secondeContainer}>
-                            <h1>HI THERE!</h1>
-                            <h2>I&#39;m Fred Nobre , a full-Stack developper<br/> based in<br/> France.</h2>
-
-                            <Image className={styles.turtel}
-                                   src={"/assets/turtelWithbackground.svg"}
-                                   alt={"fred turtle"}
-                                   fill
-                                   sizes="(max-width: 768px) 100vw, 50vw"/>
-
-                        </div>
+                    <div className={styles.secondeContainer}>
+                        <h1>{portfolioContent.Hero.title}</h1>
+{
+                        <h2>{portfolioContent.Hero.subtitle}</h2>
+}
+                    </div>
+                    <div className={styles.turtleContainer}>
+                      { <img className={styles.turtel}
+                               src={portfolioContent.Hero.images}
+                               alt={"fred turtle"}/>}
                     </div>
                 </figure>
             </header>
         </>
     )
 }
+/*todo Régler le souci du responsive avec le composant hero
+*  Je dois savoir si je mets la tortue dans le composant ou non, et dans quels des cas je la ferais disparaitre*/
