108414b4e9f09fd939cff84209994ad099a989a5
Fred Nobre <frednobre18@gmail.com>
2025-09-08 20:54:48 +0200

    :seedling: Update Hero tsx

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,26 +1,19 @@
 import styles from './Hero.module.css'
-import { portfolioContent } from "@/content/portfolioContent";
+import {portfolioContent} from "@/content/portfolioContent";
 
 export default function Hero() {
     return (
         <>
             <header className={styles.container}>
                 <figure className={styles.imageWrapper}>
-                    <div className={styles.secondeContainer}>
-                        <h1>{portfolioContent.Hero.title}</h1>
-{
-                        <h2>{portfolioContent.Hero.subtitle}</h2>
-}
-                    </div>
-                    <div className={styles.turtleContainer}>
-                      { <img className={styles.turtel}
-                               src={portfolioContent.Hero.images}
-                               alt={"fred turtle"}/>}
-                    </div>
+                    <h1 className={styles.title}>{portfolioContent.Hero.title}</h1>
+                    <h2 className ={styles.subtitle}>{portfolioContent.Hero.subtitle}</h2>
+
+                    <img className={styles.turtel}
+                         src={portfolioContent.Hero.images}
+                         alt={"fred turtle"}/>
                 </figure>
             </header>
         </>
     )
 }
-/*todo Régler le souci du responsive avec le composant hero
-*  Je dois savoir si je mets la tortue dans le composant ou non, et dans quels des cas je la ferais disparaitre*/
