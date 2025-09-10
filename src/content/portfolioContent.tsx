59481b10f01c48bc558c6b189fb8c741c5784245
Fred Nobre <frednobre18@gmail.com>
2025-09-10 19:36:29 +0200

    :lipstick: add update

--- src/content/portfolioContent.tsx
+++ src/content/portfolioContent.tsx
@@ -1,8 +1,8 @@
-import {IPortFolioHeroContent} from "@/interface/IPortFolioHeroContent";
+import {IPortFolioHeroContent} from "@/interface/iPortFolioHeroContent";
 
 export const portfolioContent:IPortFolioHeroContent = {
     Hero: {
-        title: "Hi THERE !",
+        title: "HI THERE !",
         subtitle: " \"I'm Fred Nobre,a full-stack developer\n based in France.\"",
         images: "/assets/turtelWithbackground.svg"
     },
