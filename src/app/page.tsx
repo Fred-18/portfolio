d0f8a60782b61c4e709fb60eb5b00cfef4cb84b4
Fred Nobre <frednobre18@gmail.com>
2025-09-10 19:36:49 +0200

    :construction: wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Hero from "../../components/hero/Hero";
 import Test from "../../components/testdoc/test";
+import AboutMe from "../../components/aboutme/AbouteMe";
 export default function Home() {
     return (
         <>
             <Hero/>
-{/*
-            <Test/>
-*/}
+            <AboutMe/>
         </>
     );
 }
