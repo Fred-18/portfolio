53be0d5e32eff0cf3bbc473983136487997a5a73
Fred Nobre <frednobre18@gmail.com>
2025-09-17 14:10:28 +0200

    :art: update card

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,12 +1,15 @@
 import {cardContent} from "@/content/cardContent";
 import styles from './Card.module.css';
-export default function Card(){
+
+export default function Card() {
 
     return (
         <>
             <div className={styles.Card__container}>
-                <h2 className={styles.card__title}>{cardContent.Card.title[0]}</h2>
                 <img className={styles.Card__images} src={cardContent.Card.images.src[0]}/>
+                <div className={styles.card__title__container}>
+                    <h3 className={styles.card__title}>{cardContent.Card.title[0]}</h3>
+                </div>
             </div>
         </>
     )
