00d74d2e68f983224417d15ab151d3e10e1e5c1d
Fred Nobre <frednobre18@gmail.com>
2025-09-17 14:13:44 +0200

    :pencil2: typo

--- src/content/portfolioContent.tsx
+++ src/content/portfolioContent.tsx
@@ -3,7 +3,7 @@ import {IPortFolioHero} from "@/interface/iPortFolioHero";
 export const portfolioContent:IPortFolioHero = {
     Hero: {
         title: "HI THERE !",
-        subtitle: "\I 'm Fred Nobre,a full-stack developer\n based in France.\"",
+        subtitle: "\I ' m Fred Nobre,a full-stack developer\n based in France.\"",
         images: "/assets/turtelWithbackground.svg"
     },
 };
