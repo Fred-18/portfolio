afa608e0c6f2461fb7e93caf2e39126937f9de71
Fred Nobre <frednobre18@gmail.com>
2025-09-19 09:50:44 +0200

    :art: update Tools.tsx

--- components/tools/Tools.tsx
+++ components/tools/Tools.tsx
@@ -1,6 +1,7 @@
 import Card from "../card/Card";
 import styles from './Tools.module.css';
 import {toolsContent} from "@/content/toolsContent";
+import {cardContent} from "@/content/cardContent";
 
 export default function Tools() {
 
@@ -8,7 +9,8 @@ export default function Tools() {
         <>
             <h2 className={styles.Tools__title}>{toolsContent.Tools.title}</h2>
             <div className={styles.Tools__container}>
-                <Card/>
+                {cardContent.Card.map((item)=>( <Card  title={item.title }images={item.images}/>
+                ))}
             </div>
         </>
     )
