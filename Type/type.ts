10292ac8c24dbb94ae94dfaf3e563c7a59e2549f
Fred Nobre <frednobre18@gmail.com>
2025-09-19 09:52:03 +0200

    :sparkles: add new type

--- Type/type.ts
+++ Type/type.ts
@@ -1,4 +1,12 @@
- export type BurgerMenuProps = {
+export type BurgerMenuProps = {
     isOpen: boolean;
     onToggleAction: () => void;
+}
+export type CardItem = {
+    title: string;
+    images: string;
+    id?: string;
+}
+export type icard = {
+    card: CardItem[];
 }
