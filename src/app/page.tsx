18c03eb3c9c823e300789ad9ca7ef5e7751a2888
Fred Nobre <frednobre18@gmail.com>
2025-09-23 20:17:19 +0200

    :sparkles: add new component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "../../components/hero/Hero";
 import AboutMe from "../../components/aboutme/AbouteMe";
 import Tools from '../../components/tools/Tools';
+import Project from "../../components/project/Project";
 
 export default function Home() {
     return (
@@ -8,6 +9,7 @@ export default function Home() {
             <Hero/>
             <AboutMe/>
             <Tools/>
+            <Project/>
         </>
     );
 }
