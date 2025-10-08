7898e5470f00cc6bc985f1ffeb6ec126da310b97
Fred Nobre <frednobre18@gmail.com>
2025-10-08 13:24:17 +0200

    :art: update project

--- components/project/Project.tsx
+++ components/project/Project.tsx
@@ -1,14 +1,18 @@
 import styles from './Project.module.css';
 import BigCard from "../bigCard/BigCard";
 import {bigCardContent} from "@/content/bigCardContent";
+import {projectContent} from "@/content/projectContent";
 
 export default function Project() {
     return (
         <>
             <h2 className={styles.Project__title}>Project</h2>
+            <p className={styles.Project__message}>{projectContent.Project.p}</p>
             <section className={styles.Project__container}>
                 <div className={styles.bigCard__items}>
-                    {bigCardContent.BigCard.map((item)=>(<BigCard key={item.id} description={item.p} link={item.link} techno={item.span} title={item.title}/>))}
+                    {bigCardContent.BigCard.map((item) => (
+                        <BigCard key={item.id} description={item.p} link={item.link} techno={item.span}
+                                 title={item.title}/>))}
                 </div>
             </section>
         </>
---
+++ src/content/projectContent.tsx
@@ -0,0 +1,7 @@
+import {iProject} from "@/interface/iProject";
+
+export const projectContent: iProject = {
+    Project: {
+        p: "This section is dedicated to my projects. Some were created during my learning journey, others to get familiar with a technology, understand it better, or simply practice in preparation for missions where these technologies are used. Some projects are therefore not finished — the main goal is to learn. I ask for your kindness — be strict with the code, not with the man 😉"
+    }
+}
