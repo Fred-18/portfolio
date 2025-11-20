27c9eb4ab11cf9d1828d2dbe2146099b49e86f1a
Fred Nobre <frednobre18@gmail.com>
2025-11-20 10:32:22 +0100

    :sparkles: new components CertificationItems

---
+++ components/certificationItem/CertificationItems.tsx
@@ -0,0 +1,17 @@
+import styles from '../certificationItem/CertificationItems.module.css';
+import {iCertificate} from "@/interface/iCertificate";
+
+export default function CertificationItems({title, date, school,image}: iCertificate) {
+
+    return (
+        <>
+            <article className={styles.certificate__container}>
+                <h4 className={styles.certificate__card__title}>{title}</h4>
+                <time className={styles.certificate__card__date}>{date}</time>
+                <span className={styles.certificate__card__school}>{school}</span>
+                <img src={image} alt="Certification" className={styles.certificate__card__image}/>
+
+            </article>
+        </>
+    )
+}
