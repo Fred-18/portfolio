e356c0e330972da9200fa4c07eb0c46cdf9112e9
Fred Nobre <frednobre18@gmail.com>
2025-11-20 10:28:52 +0100

    :lipstick: rename bubbleContainer

--- components/bubble/Bubble.tsx
+++ components/bubble/Bubble.tsx
@@ -3,7 +3,7 @@ import {BubbleProps} from "../../Type/type";
 export default function Bubble({projectId,projectTechno}:BubbleProps){
     return(
         <>
-            <div className={styles.parent}>
+            <div className={styles.bubblesContainer}>
             <section className={styles.bubblesContent}>{projectTechno}{projectId}</section>
             </div>
         </>
