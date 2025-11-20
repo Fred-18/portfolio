dc0f6f4ea67743cc2a2677885529cce3425f3fa7
Fred Nobre <frednobre18@gmail.com>
2025-11-20 10:19:15 +0100

    :art: rename Aboutme container

--- components/aboutme/AbouteMe.tsx
+++ components/aboutme/AbouteMe.tsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
     return (
         <>
             <section className={styles.sectionAboutMe}>
-                <div className={styles.section__title__p}>
+                <div className={styles.section__container}>
                 <h2 className={styles.section__title}>{aboutMeContent.AboutMe.title}</h2>
                 <p className={styles.section__p}>{aboutMeContent.AboutMe.p}</p>
                 </div>
