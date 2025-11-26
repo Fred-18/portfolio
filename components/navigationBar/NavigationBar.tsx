12debd4880bb73d814889de33d721b42159b4d07
fred <frednobre18@gmail.com>
2025-11-26 16:08:38 +0100

    update navbar

--- components/navigationBar/NavigationBar.tsx
+++ components/navigationBar/NavigationBar.tsx
@@ -1,30 +1,72 @@
-"use client"
-import styles from "./Navigation.module.css"
+"use client";
+import styles from "./Navigation.module.css";
 import BurgerMenu from "../burgerMenu/BurgerMenu";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export default function NavigationBar() {
-    const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
 
-    const updateNavbarState = () => {
-        setMenuIsOpen(prev => !prev)
-    }
+  const updateNavbarState = () => {
+    setMenuIsOpen((prev) => !prev);
+  };
 
-    return <>
-        <nav>
-            <div className={styles.bm}><BurgerMenu isOpen={menuIsOpen} onToggleAction={updateNavbarState}/></div>
-            <div className={styles.smallHeader}>
-                <img className={styles.turtleLogo} src={"./assets/turtelWithbackground.svg"}/>
-                <a className={styles.linkHome} href={"home"}><span>Fred Nobre</span></a>
-            </div>
-            <ul className={`${styles.primaryNavigation} ${menuIsOpen ? styles.open : ''}`}>
-                <li><a className={styles.link} href={"aboutMe"}><span>About me</span></a></li>
-                <li><a className={styles.link} href={"projects"}><span>Projects</span></a></li>
-                <li><a className={styles.link} href={"Contact"}><span>Contact</span></a></li>
-                <li><a className={styles.link} href={"skills"}><span>Skills</span></a></li>
-                <li><a className={styles.link} href={"tools"}><span>Tools</span></a></li>
-                <li><a className={styles.link} href={"certificates"}><span>Certificates</span></a></li>
-            </ul>
-        </nav>
-    </>;
-};
+  return (
+    <>
+      <nav>
+        <div className={styles.bm}>
+          <BurgerMenu isOpen={menuIsOpen} onToggleAction={updateNavbarState} />
+        </div>
+        <div className={styles.smallHeader}>
+          <img
+            className={styles.turtleLogo}
+            src={"./assets/turtelWithbackground.svg"}
+          />
+          <a className={styles.linkHome} href={"home"}>
+            <span>Fred Nobre</span>
+          </a>
+        </div>
+        <ul
+          className={`${styles.primaryNavigation} ${
+            menuIsOpen ? styles.open : ""
+          }`}
+        >
+          <li>
+            <a className={styles.link} href={"aboutMe"}>
+              <span>About me</span>
+            </a>
+          </li>
+          <li>
+            <a className={styles.link} href={"projects"}>
+              <span>Projects</span>
+            </a>
+          </li>
+          <li>
+            <a className={styles.link} href={"Contact"}>
+              <span>Contact</span>
+            </a>
+          </li>
+          <li>
+            <a className={styles.link} href={"skills"}>
+              <span>Skills</span>
+            </a>
+          </li>
+          <li>
+            <a className={styles.link} href={"tools"}>
+              <span>Tools</span>
+            </a>
+          </li>
+          <li>
+            <a className={styles.link} href={"certificates"}>
+              <span>Certificates</span>
+            </a>
+          </li>
+          <li>
+            <a className={styles.link} href={"professionaExperience"}>
+              <span>Professiona experience</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
