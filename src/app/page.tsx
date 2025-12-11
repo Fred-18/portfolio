9ea7a2bb17d35815e37d5066641a24e5b380ca52
fred <frednobre18@gmail.com>
2025-12-11 17:34:32 +0100

    test page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,19 @@ import Tools from "../../components/tools/Tools";
 import Project from "../../components/project/Project";
 import Certificate from "../../components/certificate/Certificate";
 import ProfessionalExperience from "../../components/professionalExperience/ProfessionalExperience";
+import Testimonials from "../../components/testimonials/Testimonials";
 
 export default function Home() {
-    return (
-        <>
-            <Hero/>
-            <AboutMe/>
-            <Tools/>
-            <Project/>
-            <Certificate/>
-            <ProfessionalExperience/>
-        </>
-    );
+  return (
+    <>
+      <Hero />
+      <AboutMe />
+      <Tools />
+      <Project />
+      <Certificate />
+      <ProfessionalExperience />
+      <Testimonials />
+      <h1>wwwww</h1>
+    </>
+  );
 }
