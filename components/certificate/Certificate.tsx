c7b192b8991158dc60e1225e4dadcefb2c4fa845
fred <frednobre18@gmail.com>
2025-12-11 16:01:44 +0100

    update certificate

--- components/certificate/Certificate.tsx
+++ components/certificate/Certificate.tsx
@@ -1,15 +1,23 @@
-import styles from './Certificate.module.css';
-import {certificateContent} from "@/content/certificateContent";
+import styles from "./Certificate.module.css";
+import { certificateContent } from "../../src/content/certificateContent";
 import CertificationItems from "../certificationItem/CertificationItems";
 
 export default function Certificate() {
-    return (
-        <>
-            <h2 className={styles.certificate__title}>Certificates</h2>
-            <section className={styles.certificate__section}>{certificateContent.map((value)=>(
-            <CertificationItems key={value.id} id={""} title={value.title} date={value.date} school={value.school} image={value.image}/>
-            ))}
-            </section>
-        </>
-    )
+  return (
+    <>
+      <h2 className={styles.certificate__title}>Certificates</h2>
+      <section className={styles.certificate__section}>
+        {certificateContent.map((value) => (
+          <CertificationItems
+            key={value.id}
+            id={""}
+            title={value.title}
+            date={value.date}
+            school={value.school}
+            image={value.image}
+          />
+        ))}
+      </section>
+    </>
+  );
 }
