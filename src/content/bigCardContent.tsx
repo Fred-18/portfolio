9f31817f165754ea0232acdf626aab8ed6fea9a9
fred <frednobre18@gmail.com>
2025-12-11 17:37:59 +0100

    update bigCardContent.tsx

--- src/content/bigCardContent.tsx
+++ src/content/bigCardContent.tsx
@@ -1,62 +1,78 @@
-import {iBigCard} from "@/interface/iBigCard";
+import { iBigCard } from "../interface/iBigCard";
 
 export const bigCardContent: iBigCard = {
-    BigCard: [
-        {
-            projectId: "1",
-            projectTitle: "Pokedex",
-            projectDescription: "A small project to practice React and API integration.",
-            projectTechnology: ["React", "JavaScript", "HTML", "CSS"],
-            projectLink: "https://github.com/Fred-18/pokedex?tab=readme-ov-file#pok%C3%A9dex-project-with-react-framework"
-        },
-        {
-            projectId: "2",
-            projectTitle: "Tv-show",
-            projectDescription: "A React project made under mentorship to practice API integration.",
-            projectTechnology: ["React", "JavaScript", "HTML", "CSS"],
-            projectLink: "https://github.com/Fred-18/tv-show?tab=readme-ov-file#getting-started-with-create-react-app"
-        },
-        {
-            projectId: "3",
-            projectTitle: "Todo",
-            projectDescription: "A backend Todo List app built with Java and Spring Boot to practice RESTful APIs.",
-            projectTechnology: ["Java", "Spring Boot"],
-            projectLink: "https://github.com/Fred-18/todo?tab=readme-ov-file#-todo-list-api--spring-boot-project"
-        },
-        {
-            projectId: "4",
-            projectTitle: "Etl-NBA",
-            projectDescription: "A NestJS ETL pipeline that extracts, transforms, and loads NBA data into PostgreSQL.",
-            projectTechnology: ["TypeScript", "Docker", "NestJS","PostgreSQL", ],
-            projectLink: "https://github.com/Fred-18/etl-nba?tab=readme-ov-file#nba-etl---extract-transform-and-load-project-with-nestjs"
-        },
-        {
-            projectId: "5",
-            projectTitle: "Contact-Manager",
-            projectDescription: "A console-based Java app to manage contacts stored in a CSV file.",
-            projectTechnology: ["Java"],
-            projectLink: "https://github.com/Fred-18/Contact-manager?tab=readme-ov-file#-gestionnaire-de-contacts-en-java"
-        },
-        {
-            projectId: "6",
-            projectTitle: "Salon Bio",
-            projectDescription: "A study project exploring RESTful APIs and web apps connected to a database.",
-            projectTechnology: ["Ejs", "javaScript", "Nodejs", "Expressjs"],
-            projectLink: "https://github.com/Fred-18/Salonbeautbio/blob/main/README.md#-salon-bio"
-        },
-        {
-            projectId: "7",
-            projectTitle: "La Bibliothèque",
-            projectDescription: "A full-stack study project with Spring Boot (API + MySQL) and a responsive React front-end.",
-            projectTechnology: ["Java", "Spring Boot", "Docker", "MySQL"],
-            projectLink: "https://github.com/Fred-18/Labibliotheque?tab=readme-ov-file#-la-biblioth%C3%A8que"
-        },
-        {
-            projectId: "8",
-            projectTitle: "Notomatic",
-            projectDescription: "A small mentoring project to learn React and Redux through a simple to-do app.",
-            projectTechnology: ["React", "Redux", "JavaScript", "HTML", "CSS"],
-            projectLink: "https://github.com/Fred-18/notomatic/blob/001-starter/README.md"
-        }
-    ]
-}
+  BigCard: [
+    {
+      projectId: "1",
+      projectTitle: "Pokedex",
+      projectDescription:
+        "A small project to practice React and API integration.",
+      projectTechnology: ["React", "JavaScript", "HTML", "CSS"],
+      projectLink:
+        "https://github.com/Fred-18/pokedex?tab=readme-ov-file#pok%C3%A9dex-project-with-react-framework"
+    },
+    {
+      projectId: "2",
+      projectTitle: "Tv-show",
+      projectDescription:
+        "A React project made under mentorship to practice API integration.",
+      projectTechnology: ["React", "JavaScript", "HTML", "CSS"],
+      projectLink:
+        "https://github.com/Fred-18/tv-show?tab=readme-ov-file#getting-started-with-create-react-app"
+    },
+    {
+      projectId: "3",
+      projectTitle: "Todo",
+      projectDescription:
+        "A backend Todo List app built with Java and Spring Boot to practice RESTful APIs.",
+      projectTechnology: ["Java", "Spring Boot"],
+      projectLink:
+        "https://github.com/Fred-18/todo?tab=readme-ov-file#-todo-list-api--spring-boot-project"
+    },
+    {
+      projectId: "4",
+      projectTitle: "Etl-NBA",
+      projectDescription:
+        "A NestJS ETL pipeline that extracts, transforms, and loads NBA data into PostgreSQL.",
+      projectTechnology: ["TypeScript", "Docker", "NestJS", "PostgreSQL"],
+      projectLink:
+        "https://github.com/Fred-18/etl-nba?tab=readme-ov-file#nba-etl---extract-transform-and-load-project-with-nestjs"
+    },
+    {
+      projectId: "5",
+      projectTitle: "Contact-Manager",
+      projectDescription:
+        "A console-based Java app to manage contacts stored in a CSV file.",
+      projectTechnology: ["Java"],
+      projectLink:
+        "https://github.com/Fred-18/Contact-manager?tab=readme-ov-file#-gestionnaire-de-contacts-en-java"
+    },
+    {
+      projectId: "6",
+      projectTitle: "Salon Bio",
+      projectDescription:
+        "A study project exploring RESTful APIs and web apps connected to a database.",
+      projectTechnology: ["Ejs", "javaScript", "Nodejs", "Expressjs"],
+      projectLink:
+        "https://github.com/Fred-18/Salonbeautbio/blob/main/README.md#-salon-bio"
+    },
+    {
+      projectId: "7",
+      projectTitle: "La Bibliothèque",
+      projectDescription:
+        "A full-stack study project with Spring Boot (API + MySQL) and a responsive React front-end.",
+      projectTechnology: ["Java", "Spring Boot", "Docker", "MySQL"],
+      projectLink:
+        "https://github.com/Fred-18/Labibliotheque?tab=readme-ov-file#-la-biblioth%C3%A8que"
+    },
+    {
+      projectId: "8",
+      projectTitle: "Notomatic",
+      projectDescription:
+        "A small mentoring project to learn React and Redux through a simple to-do app.",
+      projectTechnology: ["React", "Redux", "JavaScript", "HTML", "CSS"],
+      projectLink:
+        "https://github.com/Fred-18/notomatic/blob/001-starter/README.md"
+    }
+  ]
+};
