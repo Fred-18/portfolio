b9182bea2189acf0401c1ce3d686afaf58ec2c21
fred <frednobre18@gmail.com>
2025-12-11 17:18:49 +0100

    update tool components

--- components/tools/Tools.tsx
+++ components/tools/Tools.tsx
@@ -1,17 +1,21 @@
 import Card from "../card/Card";
-import styles from './Tools.module.css';
-import {toolsContent} from "@/content/toolsContent";
-import {cardContent} from "@/content/cardContent";
+import styles from "./Tools.module.css";
+import { toolsContent } from "../../src/content/toolsContent";
+import { cardContent } from "../../src/content/cardContent";
 
 export default function Tools() {
-
-    return (
-        <>
-            <h2 className={styles.Tools__title}>{toolsContent.Tools.title}</h2>
-            <div className={styles.Tools__container}>
-                {cardContent.Card.map((item)=>( <Card key={item.cardId} cardTitle={item.cardTitle } cardImages={item.cardImages}/>
-                ))}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <h2 className={styles.Tools__title}>{toolsContent.Tools.title}</h2>
+      <div className={styles.Tools__container}>
+        {cardContent.Card.map((item) => (
+          <Card
+            key={item.cardId}
+            cardTitle={item.cardTitle}
+            cardImages={item.cardImages}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
