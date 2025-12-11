fae23bab0f369ae5fef86a3694ce4f35c12d0b12
fred <frednobre18@gmail.com>
2025-12-11 16:12:18 +0100

    update hero.tsx

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,19 +1,21 @@
-import styles from './Hero.module.css'
-import {portfolioContent} from "@/content/portfolioContent";
+import styles from "./Hero.module.css";
+import { portfolioContent } from "../../src/content/portfolioContent";
 
 export default function Hero() {
-    return (
-        <>
-            <header className={styles.container}>
-                <figure className={styles.imageWrapper}>
-                    <h1 className={styles.title}>{portfolioContent.Hero.title}</h1>
-                    <h2 className ={styles.subtitle}>{portfolioContent.Hero.subtitle}</h2>
+  return (
+    <>
+      <header className={styles.container}>
+        <figure className={styles.imageWrapper}>
+          <h1 className={styles.title}>{portfolioContent.Hero.title}</h1>
+          <h2 className={styles.subtitle}>{portfolioContent.Hero.subtitle}</h2>
 
-                    <img className={styles.turtle}
-                         src={portfolioContent.Hero.images}
-                         alt={"fred turtle"}/>
-                </figure>
-            </header>
-        </>
-    )
+          <img
+            className={styles.turtle}
+            src={portfolioContent.Hero.images}
+            alt={"fred turtle"}
+          />
+        </figure>
+      </header>
+    </>
+  );
 }
