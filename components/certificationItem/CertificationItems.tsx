f7fff51fba05ed5ea6de11499b97c8640504e6bb
fred <frednobre18@gmail.com>
2025-12-11 16:04:58 +0100

    update certificateItem

--- components/certificationItem/CertificationItems.tsx
+++ components/certificationItem/CertificationItems.tsx
@@ -1,17 +1,24 @@
-import styles from '../certificationItem/CertificationItems.module.css';
-import {iCertificate} from "@/interface/iCertificate";
+import styles from "../certificationItem/CertificationItems.module.css";
+import { iCertificate } from "../../interface/iCertificate";
 
-export default function CertificationItems({title, date, school,image}: iCertificate) {
-
-    return (
-        <>
-            <article className={styles.certificate__container}>
-                <h4 className={styles.certificate__card__title}>{title}</h4>
-                <time className={styles.certificate__card__date}>{date}</time>
-                <span className={styles.certificate__card__school}>{school}</span>
-                <img src={image} alt="Certification" className={styles.certificate__card__image}/>
-
-            </article>
-        </>
-    )
+export default function CertificationItems({
+  title,
+  date,
+  school,
+  image
+}: iCertificate) {
+  return (
+    <>
+      <article className={styles.certificate__container}>
+        <h4 className={styles.certificate__card__title}>{title}</h4>
+        <time className={styles.certificate__card__date}>{date}</time>
+        <span className={styles.certificate__card__school}>{school}</span>
+        <img
+          src={image}
+          alt="Certification"
+          className={styles.certificate__card__image}
+        />
+      </article>
+    </>
+  );
 }
