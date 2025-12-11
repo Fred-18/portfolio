93a5283f0f3287086e39ce75af482890db0cc115
fred <frednobre18@gmail.com>
2025-12-11 17:39:55 +0100

    update cardContent.tsx

--- src/content/cardContent.tsx
+++ src/content/cardContent.tsx
@@ -1,52 +1,52 @@
-import {iCard} from "@/interface/iCard";
+import { iCard } from "../interface/iCard";
 
 export const cardContent: iCard = {
-    Card: [
-        {
-            cardId: "1",
-            cardTitle: "Java",
-            cardImages: "/assets/java_logo_clean.png"
-        },
-        {
-            cardId: "2",
-            cardTitle: "JavaScript",
-            cardImages: "/assets/javascriptlogo.png"
-        },
-        {
-            cardId: "3",
-            cardTitle: "Springboot",
-            cardImages: "/assets/Springboot.png "
-        },
-        {
-            cardId: "4",
-            cardTitle: "React",
-            cardImages: "/assets/Reactlogo.png"
-        },
-        {
-            cardId: "5",
-            cardTitle: "Next.js",
-            cardImages: "/assets/Nextjslogo.png"
-        },
-        {
-            cardId: "6",
-            cardTitle: "TypesScript",
-            cardImages: "/assets/Typescript.png"
-        },
-        {
-            cardId: "7",
-            cardTitle: "Express.js",
-            cardImages: "/assets/Expresslogo.png"
-        },
-        {
-            cardId: "8",
-            cardTitle: "Docker",
-            cardImages: "/assets/docker.png"
-        },
+  Card: [
+    {
+      cardId: "1",
+      cardTitle: "Java",
+      cardImages: "/assets/java_logo_clean.png"
+    },
+    {
+      cardId: "2",
+      cardTitle: "JavaScript",
+      cardImages: "/assets/javascriptlogo.png"
+    },
+    {
+      cardId: "3",
+      cardTitle: "Springboot",
+      cardImages: "/assets/Springboot.png "
+    },
+    {
+      cardId: "4",
+      cardTitle: "React",
+      cardImages: "/assets/Reactlogo.png"
+    },
+    {
+      cardId: "5",
+      cardTitle: "Next.js",
+      cardImages: "/assets/Nextjslogo.png"
+    },
+    {
+      cardId: "6",
+      cardTitle: "TypesScript",
+      cardImages: "/assets/Typescript.png"
+    },
+    {
+      cardId: "7",
+      cardTitle: "Express.js",
+      cardImages: "/assets/Expresslogo.png"
+    },
+    {
+      cardId: "8",
+      cardTitle: "Docker",
+      cardImages: "/assets/docker.png"
+    },
 
-        {
-            cardId: "9",
-            cardTitle: "Figma",
-            cardImages: "/assets/Figma.png"
-        },
-    ]
-}
+    {
+      cardId: "9",
+      cardTitle: "Figma",
+      cardImages: "/assets/Figma.png"
+    }
+  ]
+};
