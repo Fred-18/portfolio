e420ac4ff4cdb4db531039d268bf6f15d482ec71
fred <frednobre18@gmail.com>
2025-12-11 15:58:35 +0100

    update burgerMenu

--- components/burgerMenu/BurgerMenu.tsx
+++ components/burgerMenu/BurgerMenu.tsx
@@ -1,27 +1,35 @@
-"use client"
-import styles from './BurgerMenu.module.css'
+"use client";
+import styles from "./BurgerMenu.module.css";
 import Line from "../line/Line";
-import {JSX} from "react";
-import {BurgerMenuProps} from "../../Type/type";
+import { JSX } from "react";
+import { BurgerMenuProps } from "../../Type/type";
 
-export default function BurgerMenu({isOpen,onToggleAction}:BurgerMenuProps) {
+export default function BurgerMenu({
+  isOpen,
+  onToggleAction
+}: BurgerMenuProps) {
+  const isToggleMenu = () => {
+    onToggleAction();
+  };
 
-    const isToggleMenu = () => {
-        onToggleAction()
+  const repeatLines = () => {
+    const lines: JSX.Element[] = [];
+    for (let i = 0; i < 3; i++) {
+      lines.push(<Line key={i} />);
     }
+    return lines;
+  };
 
-    const repeatLines = () => {
-        const lines: JSX.Element[] = [];
-        for (let i = 0; i < 3; i++) {
-            lines.push(<Line key={i}/>);
-        }
-        return lines
-    };
-
-    return (<>
-        <button onClick={isToggleMenu} className={styles.mobileNavToggle} aria-controls={"primary-navigation"}
-                aria-expanded={isOpen}>
-            {isOpen ? <span className={styles.closeIcon}>X</span> : repeatLines()}
-        </button>
-    </>)
+  return (
+    <>
+      <button
+        onClick={isToggleMenu}
+        className={styles.mobileNavToggle}
+        aria-controls={"primary-navigation"}
+        aria-expanded={isOpen}
+      >
+        {isOpen ? <span className={styles.closeIcon}>X</span> : repeatLines()}
+      </button>
+    </>
+  );
 }
