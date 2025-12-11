4a9f83187abf0b3040d330e09b8a07a48978dcef
fred <frednobre18@gmail.com>
2025-12-11 17:45:21 +0100

    update portfolioContent.tsx

--- src/content/portfolioContent.tsx
+++ src/content/portfolioContent.tsx
@@ -1,9 +1,9 @@
-import {IPortFolioHero} from "@/interface/iPortFolioHero";
+import { IPortFolioHero } from "../interface/iPortFolioHero";
 
-export const portfolioContent:IPortFolioHero = {
-    Hero: {
-        title: "HI THERE !",
-        subtitle: "\I ' m Fred Nobre,a full-stack developer\n based in France.\"",
-        images: "/assets/turtelWithbackground.svg"
-    },
+export const portfolioContent: IPortFolioHero = {
+  Hero: {
+    title: "HI THERE !",
+    subtitle: "I ' m Fred Nobre,a full-stack developer\n based in France.\"",
+    images: "/assets/turtelWithbackground.svg"
+  }
 };
