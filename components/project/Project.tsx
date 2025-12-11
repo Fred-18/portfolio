58786d9fc5267cd9c2d1785908609ff92be77b73
fred <frednobre18@gmail.com>
2025-12-11 16:22:11 +0100

    update Project

--- components/project/Project.tsx
+++ components/project/Project.tsx
@@ -1,20 +1,27 @@
-import styles from './Project.module.css';
+import styles from "./Project.module.css";
 import BigCard from "../bigCard/BigCard";
-import {bigCardContent} from "@/content/bigCardContent";
-import {projectContent} from "@/content/projectContent";
+import { bigCardContent } from "../../src/content/bigCardContent";
+import { projectContent } from "../../src/content/projectContent";
 
 export default function Project() {
-    return (
-        <>
-            <h2 className={styles.Project__title}>Project</h2>
-            <p className={styles.Project__message}>{projectContent.Project.p}</p>
-            <section className={styles.Project__container}>
-                <div className={styles.bigCard__items}>
-                    {bigCardContent.BigCard.map((item) => (
-                        <BigCard  projectId={""} key={item.projectId} projectDescription={item.projectDescription} projectLink={item.projectLink} projectTechno={item.projectTechnology}
-                                 projectTitle={item.projectTitle}/>))}
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <h2 className={styles.Project__title}>Project</h2>
+      <p className={styles.Project__message}>{projectContent.Project.p}</p>
+      <section className={styles.Project__container}>
+        <div className={styles.bigCard__items}>
+          {bigCardContent.BigCard.map((item) => (
+            <BigCard
+              projectId={""}
+              key={item.projectId}
+              projectDescription={item.projectDescription}
+              projectLink={item.projectLink}
+              projectTechno={item.projectTechnology}
+              projectTitle={item.projectTitle}
+            />
+          ))}
+        </div>
+      </section>
+    </>
+  );
 }
