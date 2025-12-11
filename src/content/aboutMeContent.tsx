f7b682340fc82a6d10f0b01226d0409bb587b7ff
fred <frednobre18@gmail.com>
2025-12-11 17:36:17 +0100

    update aboutMe content.tsx

--- src/content/aboutMeContent.tsx
+++ src/content/aboutMeContent.tsx
@@ -1,12 +1,13 @@
-import {IAboutMe} from "@/interface/iAboutMe";
+import { IAboutMe } from "../interface/iAboutMe";
 
 export const aboutMeContent: IAboutMe = {
-    AboutMe: {
-        title: "About me",
-        p: "My name is Fred. I'm a junior developer passionate about tech and sports. " +
-            "My favorite languages are Java, JavaScript, and TypeScript. " +
-            "I mainly work with Spring Boot on the back end and React on the front end. " +
-            "I'm also interested in UX design — I'm not an expert, but I care about the people who use what I build. " +
-            "My goal? To learn, grow, build useful projects, and thrive in a team that shares the same passion.",
-    }
-}
+  AboutMe: {
+    title: "About me",
+    p:
+      "My name is Fred. I'm a junior developer passionate about tech and sports. " +
+      "My favorite languages are Java, JavaScript, and TypeScript. " +
+      "I mainly work with Spring Boot on the back end and React on the front end. " +
+      "I'm also interested in UX design — I'm not an expert, but I care about the people who use what I build. " +
+      "My goal? To learn, grow, build useful projects, and thrive in a team that shares the same passion."
+  }
+};
