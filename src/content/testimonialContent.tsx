f6f1dff778d40fcb78e70689aa5e804cc1c4d495
fred <frednobre18@gmail.com>
2025-12-11 17:54:01 +0100

    add new content and new interface Testi

---
+++ src/content/testimonialContent.tsx
@@ -0,0 +1,58 @@
+import { ITestimonials } from "../interface/iTestimonials";
+
+export const testimonialContent: ITestimonials[] = [
+  {
+    id: "1",
+    firstName: "Suxue",
+    lastName: "Li",
+    role: "Software Engineer at Octo Technology",
+    message:
+      "Fred is someone who is very pleasant to work with on a daily basis. He is curious, not afraid to ask questions, and does not hesitate to express his opinion with accuracy. He is also someone who is very dedicated in general and does not give up easily.These qualities allowed him to gain the autonomy needed on the project we worked on together for six months (1Jeune1Solution), and I can only recommend Fred, both on a human and professional level.",
+    linkedinUrl: "https://www.linkedin.com/in/suxue-li-317066130/",
+    avatar: "./assets/Su.jpeg"
+  },
+
+  {
+    id: "2",
+    firstName: "Juliette",
+    lastName: "Ferrer",
+    role: "Software Engineer at Octo Technology",
+    message:
+      "I had the pleasure of working with Fred, and I can only warmly recommend him. He is a curious, committed developer who is always eager to learn. He asks relevant questions, seeks to deeply understand technical issues, and constantly improves the quality of his code. Fred also brings real positive energy to the team: he is pleasant, kind, and knows how to collaborate effectively with everyone. He already has strong technical maturity and real potential to quickly grow into roles with greater responsibility.Fred is someone who is very pleasant to work with on a daily basis. He is curious, not afraid to ask questions, and does not hesitate to express his opinion with accuracy. He is also someone who is very dedicated in general and does not give up easily.These qualities allowed him to gain the autonomy needed on the project we worked on together for six months (1Jeune1Solution), and I can only recommend Fred, both on a human and professional level.",
+    linkedinUrl: "https://www.linkedin.com/in/juliette-ferrer/",
+    avatar: "./assets/ju.jpeg"
+  },
+
+  {
+    id: "3",
+    firstName: "Guillaume",
+    lastName: "Chauvet",
+    role: "Software Engineer at Octo Technology",
+    message:
+      "I had the chance to work with Fred, and I recommend him without hesitation. He is a junior developer who is curious, committed, and constantly improving, with a real desire to understand what he is doing and why he is doing it.He asks the right questions, knows how to question himself, and consistently seeks to improve the quality of his work.Beyond his technical skills, Fred brings very positive energy to the team: caring, serious, pleasant, and always willing to learn or help. He already shows strong maturity for his level of experience and has clear potential to grow quickly.A real pleasure to collaborate with him on a daily basis.",
+    linkedinUrl: "https://www.linkedin.com/in/g-chauvet/",
+    avatar: "./assets/Gui.jpeg"
+  },
+
+  {
+    id: "4",
+    firstName: "Valentino",
+    lastName: "Suma",
+    role: "Software Engineer at Octo Technology",
+    message:
+      "Fred is a developer who stands out for his motivation and his desire to learn. He is serious, committed, and always ready to take on new challenges. What I particularly appreciate is his calm and constructive attitude: working with him is simple and pleasant. I am convinced that he has a bright future in development and that he will bring a lot to any team that places its trust in him.",
+    linkedinUrl: "https://www.linkedin.com/in/valentino-suma/",
+    avatar: "./assets/Val.jpeg"
+  },
+
+  {
+    id: "5",
+    firstName: "Gauthier",
+    lastName: "Fiorentino",
+    role: "Software Engineer at Octo Technology",
+    message:
+      "Fred is always a pleasure to work with. He has an endless thirst for knowledge, asks questions, challenges the team, and is not afraid to get hands-on with the code. And on top of that, he s a pleasure to be around on a daily basis!",
+    avatar: "./assets/Gau.jpeg",
+    linkedinUrl: "https://www.linkedin.com/in/gauthier-fiorentino/"
+  }
+];
---
+++ src/interface/iTestimonials.tsx
@@ -0,0 +1,9 @@
+export interface ITestimonials {
+  id: string;
+  firstName: string;
+  lastName: string;
+  role: string;
+  message: string;
+  avatar: string;
+  linkedinUrl: string;
+}
